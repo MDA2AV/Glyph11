5ed861924e5a5d0fd04bd9460404e1c81296b22a
github-action-benchmark <github@users.noreply.github.com>
2026-02-06 13:55:41 +0000

    add Benchmark (benchmarkdotnet) benchmark result for 0b78633ed1a9666c3278936a22a36aecd3131b36

---
+++ benchmarks/data.js
@@ -0,0 +1,102 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1770386141676,
+  "repoUrl": "https://github.com/MDA2AV/Glyph11",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "Diogo Martins",
+            "username": "MDA2AV",
+            "email": "165835485+MDA2AV@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "0b78633ed1a9666c3278936a22a36aecd3131b36",
+          "message": "Merge pull request #13 from MDA2AV/feature/bench-action\n\nAdding separate action to run benchmarks and upload artifacts",
+          "timestamp": "2026-02-06T13:49:51Z",
+          "url": "https://github.com/MDA2AV/Glyph11/commit/0b78633ed1a9666c3278936a22a36aecd3131b36"
+        },
+        "date": 1770386140821,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "Benchmarks.FlexibleParserBenchmark.Small_ROM",
+            "value": 138.34866094589233,
+            "unit": "ns",
+            "range": "± 0.3719137955494433"
+          },
+          {
+            "name": "Benchmarks.FlexibleParserBenchmark.Small_MultiSegment",
+            "value": 359.2873463630676,
+            "unit": "ns",
+            "range": "± 0.5206726816667171"
+          },
+          {
+            "name": "Benchmarks.FlexibleParserBenchmark.Header4K_ROM",
+            "value": 724.2281716664633,
+            "unit": "ns",
+            "range": "± 2.954479250951705"
+          },
+          {
+            "name": "Benchmarks.FlexibleParserBenchmark.Header4K_MultiSegment",
+            "value": 1804.792044321696,
+            "unit": "ns",
+            "range": "± 4.785500967996178"
+          },
+          {
+            "name": "Benchmarks.FlexibleParserBenchmark.Header32K_ROM",
+            "value": 4536.571072896321,
+            "unit": "ns",
+            "range": "± 1.4223982117483693"
+          },
+          {
+            "name": "Benchmarks.FlexibleParserBenchmark.Header32K_MultiSegment",
+            "value": 12114.52385965983,
+            "unit": "ns",
+            "range": "± 81.60254256535697"
+          },
+          {
+            "name": "Benchmarks.HardenedParserBenchmark.Small_ROM",
+            "value": 179.88360226154327,
+            "unit": "ns",
+            "range": "± 0.007186712020683606"
+          },
+          {
+            "name": "Benchmarks.HardenedParserBenchmark.Small_MultiSegment",
+            "value": 422.9433428446452,
+            "unit": "ns",
+            "range": "± 0.42816942297921284"
+          },
+          {
+            "name": "Benchmarks.HardenedParserBenchmark.Header4K_ROM",
+            "value": 1037.448823928833,
+            "unit": "ns",
+            "range": "± 0.3924711692212917"
+          },
+          {
+            "name": "Benchmarks.HardenedParserBenchmark.Header4K_MultiSegment",
+            "value": 2154.8385569254556,
+            "unit": "ns",
+            "range": "± 10.267421483450853"
+          },
+          {
+            "name": "Benchmarks.HardenedParserBenchmark.Header32K_ROM",
+            "value": 7375.548927307129,
+            "unit": "ns",
+            "range": "± 6.445701794007483"
+          },
+          {
+            "name": "Benchmarks.HardenedParserBenchmark.Header32K_MultiSegment",
+            "value": 15921.4045308431,
+            "unit": "ns",
+            "range": "± 158.02849148870615"
+          }
+        ]
+      }
+    ]
+  }
+}
